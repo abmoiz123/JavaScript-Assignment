1f34b59dae8540ed4648a443ae8825a906fea007
abmoiz123 <abdulmoiz328@gmail.com>
2020-06-06 23:46:04 +0500

    assignment done

---
+++ App.js
@@ -0,0 +1,148 @@
+// alert ("Hello World!");
+// alert ("Error! Please enter a valid password");
+// alert ("Welcome to JS Land\nHappy Coding!");
+// alert ("Welcome to JS Land");
+
+// var happy = ("Happy Coding!");
+// var checkBox = document.getElementById("myCheck");
+// var happy2 = (happy + checkBox)
+// alert (happy2);
+
+
+// var username = ("Moiz");
+// alert (username);
+// var Myname = ("Muhammad Moiz");
+// alert (Myname);
+// var message = ("Hello World");
+// alert (message);
+// var student = ("Student Name: M.Moiz");
+// alert(student);
+// var studentAge = ("Student Age: 20 years Old");
+// alert(studentAge);
+// var certification = ("Certification: Certified Mobile Application Development");
+// alert(certification);
+// var pizza = ("PIZZA\nPIZZ\nPIZ\nPI\nP");
+// alert(pizza);
+// var email = ("My Email Address is ");
+// var eaddress = ("abdulmoiz328@gmail.com");
+// var emailaddress = (email + eaddress);
+// alert(emailaddress);
+// var book1 = ("I am trying to learn from the book ");
+// var book = ("A Smarter way to learn JavaScript");
+// var book2 = (book1 + book);
+// alert(book2);
+// var doc = ("Yah! I can write HTML content through JavaScript");
+// document.write(doc);
+// var design = ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+// alert(design);
+
+
+// var age1 = ("I am 20 years old");
+// alert(age1);
+// var visit = ("You have visited this site 14 times");
+// alert(visit);
+// var message1 = ("My birth year is ");
+// var birthyear = (1999);
+// document.write(message1 + birthyear + "<br>");
+// var visitorname = ("M.Moiz ");
+// var producttitle = (" T-shirt(s) ");
+// var quantity = (5 );
+// document.write(visitorname + "Ordered " + quantity + producttitle + "on Fashion.online store." );
+
+
+
+// var num1 = +prompt("Addition Number 1");
+// var num2 = +prompt("Addition Number 2");
+// var num3 = num1+num2;
+// document.write("Sum of " + num1 + "and " + num2 + "is " + num3);
+
+// var num4 = +prompt("Subtraction Number 1");
+// var num5 = +prompt("Subtraction Number 2");
+// var num6 = num4 - num5;
+// document.write("Difference of" + num4 + "and " + num5 + "is " + num6);
+
+// var num7 = +prompt("Multiplication Number 1");
+// var num8 = +prompt("Multiplication Number 2");
+// var num9 = num7 * num8;
+// document.write("Product of " + num7 + "and " + num8 + "is " + num9);
+
+// var num10 = +prompt("Division Number 1");
+// var num11 = +prompt("Division Number 2");
+// var num12 = num10 / num11;
+// document.write("Quotient of " + num10 + "and " + num11 + "is " + num12);
+
+// var undef_ined = ("Value after variable declaration is undefined");
+// document.write(undef_ined + "<br>")
+// var value_1 = 5;
+// document.write("Initial value: " + value_1 + "<br>")
+// var increa_ment1 = (++value_1);
+// document.write("Value after increament is: " + increa_ment1 + "<br>");
+// var add_1 = 7;
+// var add_2 = (increa_ment1 + add_1);
+// document.write("Value after addition is: " + add_2 + "<br>")
+// var decrea_ment = (--add_2);
+// document.write("Value after decreament is: " + decrea_ment + "<br>");
+// var divi_de = (decrea_ment % 3);
+// document.write("The reminder is: " + divi_de + "<br>");
+
+// var movie_ticket = 600;
+// var quantity_1 = 5;
+// var total = 600 * 5;
+// document.write("Total cost to buy " + quantity_1 + " tickets to a movie is " + total + "PKR" + "<br>");
+// var table = ("Table of 4");
+// document.write(table + "<br>");
+// var table_value = 4;
+// var table_1 = table_value * 1;
+// document.write(table_value + " x" + " 1" + " = " + table_1 + "<br>");
+// var table_2 = table_value * 2;
+// document.write(table_value + " x" + " 2" + " = " + table_2 + "<br>");
+// var table_3 = table_value * 3;
+// document.write(table_value + " x" + " 3" + " = " + table_3 + "<br>");
+// var table_4 = table_value * 4;
+// document.write(table_value + " x" + " 4" + " = " + table_4 + "<br>");
+// var table_5 = table_value * 5;
+// document.write(table_value + " x" + " 5" + " = " + table_5 + "<br>");
+// var table_6 = table_value * 6;
+// document.write(table_value + " x" + " 6" + " = " + table_6 + "<br>");
+// var table_7 = table_value * 7;
+// document.write(table_value + " x" + " 7" + " = " + table_7 + "<br>");
+// var table_8 = table_value * 8;
+// document.write(table_value + " x" + " 8" + " = " + table_8 + "<br>");
+// var table_9 = table_value * 9;
+// document.write(table_value + " x" + " 9" + " = " + table_9 + "<br>");
+// var table_10 = table_value * 10;
+// document.write(table_value + " x" + " 10" + " = " + table_10 + "<br>");
+
+// var cTemp = 25;
+// var cToFahr = cTemp * 9 / 5 + 32;
+// var message_1 = cTemp+"\xB0C is " + cToFahr + " \xB0F.";
+// document.write(message_1 + "<br>");
+
+// var fTemp = 70;
+// var fToCel = (fTemp - 32) * 5 / 9;
+// var message_2 = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+// document.write(message_2 + "<br>");
+
+// var item_1 = 650;
+// var item_2 = 100;
+// var quantity_2 = 3;
+// var quantity_3 = 7;
+// var shipping = 100;
+// var item_total1 = item_1 * quantity_2;
+// var item_total2 = item_2 * quantity_3;
+// var item_total3 = item_total1 + item_total2 + shipping;
+// document.write("Price of item 1 is " + item_1 + "<br>");
+// document.write("Quantity of item 1 is " + quantity_2 + "<br>");
+// document.write("Price of item 2 is " + item_2 + "<br>");
+// document.write("Quantity of item 2 is " + quantity_3 + "<br>");
+// document.write("Shipping Charges " + shipping + "<br>" + "<br>");
+// document.write("Total cost of your order is " + item_total3 + "<br>")
+
+var marks_1 = 804;
+var marks_2 = 980;
+var percentage = marks_1 * 100;
+var percentage_2 = percentage / marks_2;
+document.write("Marks Sheet" + "<br>" + "<br>"+ "<br>");
+document.write("Total Marks: " + marks_2 + "<br>"); 
+document.write("Marks Obtained: " + marks_1 + "<br>"); 
+document.write("Percentage: " + percentage_2+ "<br>"); 
